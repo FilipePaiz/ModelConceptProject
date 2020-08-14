028d8d1b8ba4c49f727948908c7e6a5ed0d3f9cd
Filipe Pais <vitor.filipe.pais@gmail.com>
2020-08-14 16:40:31 +0100

    Add categories

--- src/main/java/com/udemy/curso/resources/CategoryResource.java
+++ src/main/java/com/udemy/curso/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.udemy.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.curso.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listing() {
-		return "REST IS WORKING WELL";
+	public List<Category> listing() {
+		
+		Category cat1 = new Category(1, "Computador");
+		Category cat2 = new Category(2, " Periféricos");
+		
+		List<Category> categoryList = new ArrayList<>();
+		categoryList.add(cat1);
+		categoryList.add(cat2);
+		
+		return categoryList;
 	}
 }
