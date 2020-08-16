6036fd18655be41445844c1562721f458fcc03d6
Filipe Pais <vitor.filipe.pais@gmail.com>
2020-08-16 15:41:54 +0100

    Created exceptions on controller and service level

--- src/main/java/com/udemy/curso/daos/ProductDAO.java
+++ src/main/java/com/udemy/curso/daos/ProductDAO.java
@@ -1,8 +1,6 @@
 package com.udemy.curso.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.udemy.curso.domain.Product;
 
 public interface ProductDAO extends JpaRepository<Product, Integer>{
--- src/main/java/com/udemy/curso/services/CategoryService.java
+++ src/main/java/com/udemy/curso/services/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.udemy.curso.daos.CategoryDAO;
 import com.udemy.curso.domain.Category;
+import com.udemy.curso.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,7 +17,8 @@ public class CategoryService {
 	
 	public Category search(Integer id) {
 		Optional<Category> cat = dao.findById(id);
-		return cat.orElse(null);
+		return cat.orElseThrow(() -> new ObjectNotFoundException(
+				"Item not found! Id: " + id + ", Type: " + Category.class.getName()));
 		
 	}
 }
