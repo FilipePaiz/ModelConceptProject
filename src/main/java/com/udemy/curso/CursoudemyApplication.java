be2e9040153babfa32474bd2a55c8fcff826637d
Filipe Pais <vitor.filipe.pais@gmail.com>
2020-08-16 14:31:56 +0100

    First DI operation

--- src/main/java/com/udemy/curso/CursoudemyApplication.java
+++ src/main/java/com/udemy/curso/CursoudemyApplication.java
@@ -1,13 +1,32 @@
 package com.udemy.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.udemy.curso.daos.CategoryDAO;
+import com.udemy.curso.domain.Category;
+
 @SpringBootApplication
-public class CursoudemyApplication {
+public class CursoudemyApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryDAO categoryDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoudemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Computadores");
+		Category cat2 = new Category(null, "Monitores");
+		
+		categoryDao.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
