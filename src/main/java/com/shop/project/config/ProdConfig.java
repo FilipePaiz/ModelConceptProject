e8a550e7524c43a63b87cc3fc14e30e58b143e23
Filipe Pais <vitor.filipe.pais@gmail.com>
2020-08-20 18:51:41 +0100

    change prod db

---
+++ src/main/java/com/shop/project/config/ProdConfig.java
@@ -0,0 +1,34 @@
+package com.shop.project.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.shop.project.services.DBService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!strategy.equals("create")) {
+			return false;
+		}
+			
+		dbService.InstantiateTestDatabase();
+		
+		return true;
+	}
+}
