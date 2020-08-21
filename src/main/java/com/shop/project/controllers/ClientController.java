2cae2e5fbcbcb6472a846969468fee2e870ebe8c
Filipe Pais <vitor.filipe.pais@gmail.com>
2020-08-21 18:33:22 +0100

    auth endpoints for specific users

--- src/main/java/com/shop/project/controllers/ClientController.java
+++ src/main/java/com/shop/project/controllers/ClientController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -60,7 +61,7 @@ public class ClientController {
 		return ResponseEntity.noContent().build();
 	}
 	
-
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
 	public ResponseEntity<Client> deleteClient(@PathVariable Integer id) {
 		
@@ -69,6 +70,7 @@ public class ClientController {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<ClientDTO>> findAll() {
 		
@@ -79,6 +81,7 @@ public class ClientController {
 		return ResponseEntity.ok().body(dtoList);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/page",method=RequestMethod.GET)
 	public ResponseEntity<Page<ClientDTO>> findPage(
 			@RequestParam(value="page", defaultValue="0") Integer page, 
